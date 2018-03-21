472f707be1aeeb7ce1d5d09277f8526f7b4eea88
Jose Antonio Bravo Isidro <ja.bravo.isidro@gmail.com>
2018-03-21 15:21:46 +0000

    Mapbox

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import './ReactotronConfig';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
+Mapbox.setAccessToken('pk.eyJ1IjoiYm9vbmFnZW5jeWlybCIsImEiOiJjamE3emdlZ3YwMXIxMnhxbWRqNGRxY3huIn0.RnmocFzP2hqvfi9nnTgi0A');
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+  
+  async onReady() {
+    this.map.fitBounds([-9.093649,53.282784],[-9.045685,53.270138],0,50000);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>hola</Text>
+        <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Dark}
+            zoomLevel={15}
+            centerCoordinate={[11.256, 43.770]}
+            style={styles.container}
+            logoEnabled={false}
+            ref={ref => this.map = ref}
+            onDidFinishLoadingMap={() => this.onReady()}>
+            
+        </Mapbox.MapView>
       </View>
     );
   }
@@ -14,8 +34,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
